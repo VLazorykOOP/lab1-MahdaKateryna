a4db3ac0073763824e48cda03acdbd4d9477226e
magdakaterina4@gmail.com <magdakaterina4@gmail.com>
2023-03-30 18:31:16 +0300

    тест

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -1,11 +1,9 @@
-﻿ // ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
 #include <fstream>
-#include <ios>
 #include <vector>
-
 #include <time.h>
 
 using namespace std;
@@ -13,11 +11,11 @@ using namespace std;
 typedef double* pDouble;
 /*
 *   ConsoleInputArrayDouble
-*   
+*
 */
 int ConsoleInputSizeArray(const int sizeMax)
 {
-    int size = 0; 
+    int size = 0;
     do {
         cout << " Input size Array ( 0< 1 < " << sizeMax << " ) ";
         cin >> size;
@@ -31,7 +29,7 @@ int ConsoleInputSizeArray(const int sizeMax)
 int ConsoleInputArray(int sizeMax, double A[])
 {
     int size = ConsoleInputSizeArray(sizeMax);
-        for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         cout << " Array[ " << i << "] "; cin >> A[i];
     }
     return size;
@@ -44,7 +42,7 @@ int ConsoleInputArray(int sizeMax, double A[])
 int RndInputArray(int sizeMax, double A[])
 {
     int size = ConsoleInputSizeArray(sizeMax);
-    int r1=0, r2=0;
+    int r1 = 0, r2 = 0;
     srand(size);
 
     for (int i = 0; i < size; i++) {
@@ -57,7 +55,7 @@ int RndInputArray(int sizeMax, double A[])
     return size;
 }
 
-int ConsoleInputDynamicArrayNew(int sizeMax, pDouble &pA)
+int ConsoleInputDynamicArrayNew(int sizeMax, pDouble& pA)
 {
     int size = ConsoleInputSizeArray(sizeMax);
     pA = new double[size];
@@ -79,23 +77,23 @@ int ConsoleInputDynamicArray_calloc(int sizeMax, pDouble& pA)
     return size;
 }
 
-void ConsoleInputVector(int sizeMax, vector<double> &A)
+void ConsoleInputVector(int sizeMax, vector<double>& A)
 {
     int size = ConsoleInputSizeArray(sizeMax);
     double d;
     for (int i = 0; i < size; i++) {
         cout << " Array[ " << i << "] "; cin >> d; A.push_back(d);
     }
-    return ;
+    return;
 }
 
 
 /*
-*  WriteArrayTextFile 
+*  WriteArrayTextFile
 *
 */
 
-void WriteArrayTextFile(int n, double *arr, const char *fileName )
+void WriteArrayTextFile(int n, double* arr, const char* fileName)
 {
     ofstream fout(fileName);
     if (fout.fail()) return;
@@ -117,12 +115,12 @@ int ReadArrayTextFile(int n, double* arr, const char* fileName)
     if (fin.fail()) return 0;
     fin >> size;
     if (size <= 0) return 0;
-    if (size > n) size = n;   
+    if (size > n) size = n;
     for (int i = 0; i < n; i++)
-       fin>> arr[i];
+        fin >> arr[i];
     fin.close();
     return size;
-    
+
 }
 
 
@@ -132,14 +130,14 @@ void WriteArrayBinFile(int n, double* arr, const char* fileName)
     ofstream bfout(fileName, ios_base::binary);
     if (bfout.fail()) return;
     bfout.write((const char*)&n, sizeof(int));
-    std::streamsize  cn = static_cast<std::streamsize>(n) *sizeof(double);
+    std::streamsize  cn = static_cast<std::streamsize>(n) * sizeof(double);
     bfout.write((const char*)arr, cn);
     bfout.close();
 }
 
 int ReadArrayBinFile(int n, double* arr, const char* fileName)
 {
-    int size=0;
+    int size = 0;
     ifstream bfin(fileName, ios_base::binary);
     if (bfin.fail()) return 0;
     bfin.read((char*)&size, sizeof(int));
@@ -157,18 +155,17 @@ void ShowMainMenu()
     cout << "    1.  Task 1  \n";
     cout << "    2.  Task 2  \n";
     cout << "    3.  Task 3  \n";
-  }
+}
 
 void MenuTask()
 {
     cout << "     Menu Task   \n";
     cout << "    1.  Local array  \n";
     cout << "    2.  Dynamic array 1 \n";
-    cout << "    3.  Dynamic array 2  new \n"; 
+    cout << "    3.  Dynamic array 2  new \n";
     cout << "    4.  Dynamic array : vector \n";
     cout << "    5.  Exit \n";
 }
-
 void MenuInput()
 {
     cout << "     Menu Input   \n";
@@ -178,112 +175,104 @@ void MenuInput()
     cout << "    4.  bb    \n";
     cout << "    5.  Exit \n";
 }
+const int MAX_SIZE = 400;
+void Task1()
+{
+    int n, j = 0;
+    double A[MAX_SIZE], B[MAX_SIZE];
+    n = ConsoleInputArray(MAX_SIZE, A);
+    for (int i = 0; i < n; i++)
+        if (A[i] != 0)
+            B[j++] = A[i];
 
-
-/*
-* Задано одновимірний масив А розміру 2N. 
-* Побудувати два масиви В і С розміру N, 
-* включивши  у масив В елементи масиву А з парними індексами,
-* а у С - з непарними.
-*****************
-*  A - in 
-*  B, C - out 
-*/
-void  TestVariant(int N, double* A, double* B, double* C) {
-    for (int i = 0; i < N; i++) {
-        B[i] = A[2 * i];
-        C[i] = A[2 * i + 1];
+    cout << "Array B witout zero elements: ";
+    for (int i = 0; i < j; i++) {
+        cout << B[i] << " ";
     }
-}
-/*
-*  Task  Var
-* 
-* 
-*/
-void TaskV()
-{
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = getchar();
-        getchar();
-            switch (ch) {
-             case '1': cout << " 1 "; break;
-             case '2': cout << " 2 "; break;
-            //
-            case '5': return;
-            }
-        cout << " Press any key and enter\n";
-        ch = getchar();
-        } while (ch != 27);
-    
+    cout << endl;
 }
 
-void ArrayLocal()
+void Task2()
 {
-    double A[1000], B[500], C[500];
     int n;
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = getchar();
-        getchar();
-        switch (ch) {
-        case '1': cout << " 1 "; break;
-        case '2': cout << " 2 "; break;
-            //
-        case '5': return;
+    double A[MAX_SIZE], x;
+    n = ConsoleInputArray(MAX_SIZE, A);
+    cout << "Input X: ";
+    cin >> x;
+    int max_negative = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (*(A + i) < 0 && *(A + i) > max_negative && *(A + i) < x) {
+            max_negative = *(A + i);
         }
-        cout << " Press any key and enter\n";
-        ch = getchar();
-    } while (ch != 27);
-
-}
-
-
-int main()
-{ 
-    
-    
-    
-    const int MAX_SIZE = 560;
-    std::cout << "Hello World!\n";
-    ShowMainMenu();
-    /*
-    double A[MAX_SIZE], B[MAX_SIZE],C[MAX_SIZE];
-    int n,m;
-    n = RndInputArray(MAX_SIZE, A);
-    WriteArrayTextFile(n, A, "1.txt");
-    m = ReadArrayTextFile(MAX_SIZE, B, "1.txt");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << B[i] << "   ";
-    WriteArrayBinFile(n, A, "1.bin");
-    m = ReadArrayBinFile(MAX_SIZE, C, "1.bin");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << C[i] << "   ";
-    cout << "\n  Vector \n";
-    vector<double> vA;
-    ConsoleInputVector(MAX_SIZE, vA);
-    for (auto v : vA) {
-        cout << v << "   ";
+        if (*(A + i) < x) {
+            break;
+        }
     }
-*/
-    TaskV();
-    return 1;
 
+    if (max_negative == INT_MIN) {
+        cout << "No negative elements!" << endl;
+    }
+    else {
+        cout << "Max negative element: " << max_negative << endl;
+    }
 }
+void Task3() {
+    int n;
+    double A[MAX_SIZE];
+    double sum = 0;
+    double avg;
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+    cout << "Input n (n <= 200): ";
+    cin >> n;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    cout << "Input array A(2n): " << endl;
+    for (int i = 0; i < 2 * n; i++) {
+        cout << "Array[ " << i << "] ";
+        cin >> A[i];
+        if (i < n) {
+            sum += A[i];
+        }
+    }
+
+    avg = sum / n;
+
+    sum = 0;
+    for (int i = n; i < 2 * n; i++) {
+        if (A[i] > avg) {
+            sum += A[i];
+        }
+    }
+
+
+    cout << "Sum: " << sum << endl;
+}
+int main()
+{
+    ShowMainMenu();
+    int nom;
+    do {
+        cin >> nom;
+    } while (nom < 1 || nom > 3);
+    system("CLS");
+    switch (nom)
+    {
+    case 1:
+    {
+        Task1();
+    }
+    break;
+    case 2:
+    {
+        Task2();
+    }
+    break;
+    case 3:
+    {
+        Task3();
+    }
+    break;
+    default:
+        break;
+    }
+    return 1;
+}
